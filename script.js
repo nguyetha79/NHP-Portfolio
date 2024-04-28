59aa34dcb20a8018c60b8dc4fff8d7598fce600b
nguyetha79 <nguyet-ha1.phung@stu.uni-regensburg.de>
2024-04-28 11:14:56 +0200

    add topBtn

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/** Top Button */
+let topButton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 /** Tabs */
 const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
